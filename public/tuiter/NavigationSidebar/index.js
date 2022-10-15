63aeabeda4e168ba8c8dffedc0d1fd31c85d7333
ehhaines <haines.e@northeastern.edu>
2022-10-15 17:31:14 -0400

    Complete section 3

---
+++ public/tuiter/NavigationSidebar/index.js
@@ -0,0 +1,50 @@
+const NavigationSidebar = () => {
+  return(`
+  <div class="list-group">
+    <a href="../home.html" class="list-group-item list-group-item-action">
+      <i class="fa-brands fa-twitter"></i></a>
+    <a href="../home.html" class="list-group-item list-group-item-action">
+      <div class="row">
+        <div class="col-1"><i class="fa-solid fa-house"></i></div>
+        <div class="col d-none d-xl-block">Home</div>
+      </div></a>
+    <a href="." class="list-group-item list-group-item-action active">
+      <div class="row">
+        <div class="col-1"><i class="fa-solid fa-hashtag"></i></div>
+        <div class="col d-none d-xl-block">Explore</div>
+      </div></a>
+    <a href="#" class="list-group-item list-group-item-action">
+      <div class="row">
+        <div class="col-1"><i class="fa-solid fa-bell"></i></div>
+        <div class="col d-none d-xl-block">Notifications</div>
+      </div></a>
+    <a href="#" class="list-group-item list-group-item-action">
+      <div class="row">
+        <div class="col-1"><i class="fa-solid fa-envelope"></i></div>
+        <div class="col d-none d-xl-block">Messages</div>
+      </div></a>
+    <a href="../bookmarks/index.html" class="list-group-item list-group-item-action">
+      <div class="row">
+        <div class="col-1"><i class="fa-solid fa-bookmark"></i></div>
+        <div class="col d-none d-xl-block">Bookmarks</div>
+      </div></a>
+    <a href="#" class="list-group-item list-group-item-action">
+      <div class="row">
+        <div class="col-1"><i class="fa-solid fa-list"></i></div>
+        <div class="col d-none d-xl-block">Lists</div>
+      </div></a>
+    <a href="../profile.html" class="list-group-item list-group-item-action">
+      <div class="row">
+        <div class="col-1"><i class="fa-solid fa-user"></i></div>
+        <div class="col d-none d-xl-block">Profile</div>
+      </div></a>
+    <a href="#" class="list-group-item list-group-item-action">
+      <div class="row">
+        <div class="col-1"><i class="fa-solid fa-ellipsis"></i></div>
+        <div class="col d-none d-xl-block">More</div>
+      </div></a>
+  </div>
+  <button type="button" class="btn btn-primary w-100 mt-1 rounded-pill">Tuit</button>
+  `);
+ }
+ export default NavigationSidebar;
