63c0383ae211d5eeb6e680c55ac07c3cf3b6340a
ehhaines <haines.e@northeastern.edu>
2022-10-17 13:44:21 -0400

    Complete up to and including 2.11

---
+++ src/labs/a6/index.js
@@ -0,0 +1,13 @@
+import Classes from "./classes";
+import Styles from "./styles";
+
+function Assignment6() {
+  return (
+    <div>
+      <h1>Assignment 6</h1>
+      <Classes/>
+      <Styles/>
+    </div>
+  );
+ }
+ export default Assignment6;
