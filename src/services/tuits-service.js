70367960700cf170d478b45ac8467c3fedddfc8e
ehhaines <haines.e@northeastern.edu>
2022-11-16 16:14:31 -0500

    Resolve problems

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
-const TUITS_API = 'http://localhost:4000/api/tuits';
+
+const API_BASE = process.env.REACT_APP_API_BASE;
+const TUITS_API = `${API_BASE}/tuits`;
 
 export const createTuit = async (tuit) => {
   const response = await axios.post(TUITS_API, tuit);
@@ -21,5 +23,5 @@ return response.data
 export const updateTuit = async (tuit) => {
   const response = await axios
     .put(`${TUITS_API}/${tuit._id}`, tuit);
-  return tuit;
+  return response.data;
 }
