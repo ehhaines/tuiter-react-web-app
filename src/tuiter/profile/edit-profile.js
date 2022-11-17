bb299e978415617f7c1065522447405de5c3fbab
ehhaines <haines.e@northeastern.edu>
2022-11-16 19:35:57 -0500

    Revert "Add references in comments"
    This reverts commit bf076142745d8912da96b7b752725bfbf0d0a9ca.
    
    Forgot to create a branch a8.

--- src/tuiter/profile/edit-profile.js
+++ src/tuiter/profile/edit-profile.js
@@ -55,11 +55,6 @@ const EditProfile = () => {
     dispatch(changeDOB(dob));
   }
 
-  /**
-   * I referenced this source to learn about how to redirect on onClick:
-   * https://stackoverflow.com/questions/50644976/react-button-onclick-redirect-page
-   */
-
   const nav = useNavigate()
 
   return(
