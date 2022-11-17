8519fc767df713f7a942d1479a7008f95127423d
ehhaines <haines.e@northeastern.edu>
2022-11-16 20:03:37 -0500

    Revert "Resolve problems"
    This reverts commit 70367960700cf170d478b45ac8467c3fedddfc8e.

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
-
-const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = `${API_BASE}/tuits`;
+const TUITS_API = 'http://localhost:4000/api/tuits';
 
 export const createTuit = async (tuit) => {
   const response = await axios.post(TUITS_API, tuit);
@@ -23,5 +21,5 @@ return response.data
 export const updateTuit = async (tuit) => {
   const response = await axios
     .put(`${TUITS_API}/${tuit._id}`, tuit);
-  return response.data;
+  return tuit;
 }
